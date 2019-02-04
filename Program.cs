509d1955e894d54fb0d6768088d1d68e939d51a7
nataszaplachta <natasza.plachta@gmail.com>
2019-02-04 16:55:03 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            int n = Int32.Parse(Console.ReadLine());
+        }
+    }
+}
